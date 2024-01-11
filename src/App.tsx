890a5c638e428dccc445fd01eb040ff802e24f69
DanPGill2023 <dangillsocialmediaapp@gmail.com>
2024-01-11 19:33:56 +0000

    Adds a basic search bar which validates postcodes and updates search params accordingly

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Search from "src/features/search/Search";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col min-h-screen flex-grow">
+      <Search />
+    </div>
+  );
 }
 
 export default App;
