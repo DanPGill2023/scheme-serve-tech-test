6be4b1aee13e19b27cc12faaed4c31d5b653a560
DanPGill2023 <dangillsocialmediaapp@gmail.com>
2024-01-19 11:59:05 +0000

    Solves potential issue with interpolation in tailwindcss class

--- src/components/LoadingSpinnerWrapper.tsx
+++ src/components/LoadingSpinnerWrapper.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from "react";
 
 type LoadingSpinnerWrapperProps = {
   isLoading: boolean;
-  spinnerColor: string;
+  spinnerColor?: string;
 };
 
 const LoadingSpinnerWrapper = ({
@@ -10,23 +10,13 @@ const LoadingSpinnerWrapper = ({
   isLoading,
   spinnerColor,
 }: PropsWithChildren<LoadingSpinnerWrapperProps>) => {
-  const spinnerClassNames = [
-    "inline-block",
-    "h-8",
-    "w-8",
-    "animate-spin",
-    "rounded-full",
-    "border-4",
-    "border-solid",
-    ...(spinnerColor ? [`text-${spinnerColor}`] : ["text-theme-purple"]),
-    "border-current",
-    "border-r-transparent",
-    "align-[-0.125em]",
-    "motion-reduce:animate-[spin_1.5s_linear_infinite]",
-  ].join(" ");
   return isLoading ? (
     <div className="flex flex-1 justify-center items-center">
-      <div className={spinnerClassNames} role="status"></div>
+      <div
+        style={{ color: spinnerColor }}
+        className="inline-block h-8 w-8 text-theme-purple animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em]motion-reduce:animate-[spin_1.5s_linear_infinite]"
+        role="status"
+      ></div>
     </div>
   ) : (
     <>{children}</>
